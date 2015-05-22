b5f8256aa94f13e01aecfce73a8ee950856048ea
rain_home <liu044100@gmail.com>
2015-05-22 12:37:38 +0900

    Initial Commit

---
+++ event_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  event_demo
+//
+//  Created by yuchen liu on 5/22/15.
+//  Copyright (c) 2015 rain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
